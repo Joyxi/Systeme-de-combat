98301df38312c0b81226e4d02142ac4fb90ae566
AlexisLuu <alexis.lu@outlook.fr>
2020-01-16 14:19:22 +0100

    exercice 1
    fonctionne

---
+++ version0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+	
+	int Pdv;
+	Pdv = 150;
+	printf ("Niveau de vie: %d\n",Pdv);
+	printf ("Vous avez ete touche ! -10p\n");
+	Pdv = Pdv-10;
+	printf ("Niveau de vie: %d\n",Pdv);
+	
+	
+	
+	return 0;
+	
+}
